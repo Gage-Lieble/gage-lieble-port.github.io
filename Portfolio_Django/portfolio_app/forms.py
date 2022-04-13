bf7c621a6ad14efdd8f277c47144cc49181d3da6
Gage Lieble <gagelieble@hotmail.com>
2022-04-12 19:23:55 -0700

    removed unessesary imports

--- Portfolio_Django/portfolio_app/forms.py
+++ Portfolio_Django/portfolio_app/forms.py
@@ -1,6 +1,4 @@
 from email import message
-from tkinter import Widget
-from attr import fields
 from django.forms import ModelForm, Textarea, EmailField
 from .models import ContactMe
 from django import forms
