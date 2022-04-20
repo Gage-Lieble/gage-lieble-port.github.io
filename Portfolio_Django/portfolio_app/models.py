b9388d220dd96fc6a2064365bb43a33da630ea85
Gage Lieble <gagelieble@hotmail.com>
2022-04-19 21:37:41 -0700

    Added time sent to the contact me model

--- Portfolio_Django/portfolio_app/models.py
+++ Portfolio_Django/portfolio_app/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class ContactMe(models.Model):
     email = models.EmailField()
     message = models.TextField(max_length=500)
+    sent_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Email: {self.email} / Message: {self.message} "
+        return f"Email: {self.email} / Message: {self.message} / sent: {self.sent_time}"
